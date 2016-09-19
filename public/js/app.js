7f1fb386b7ff0e50b48e0df92542e8ad6188b21d
Justin St. Arnauld <justin.starnauld@gmail.com>
2016-09-19 16:29:58 -0700

    Add timestamps to socket app

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,11 @@ socket.on('connect', function () {
 });
 
 socket.on('message', function (message) {
+  var momentTimestamp = moment.utc(message.timestamp);
   console.log('New message');
   console.log(message.text);
 
-  jQuery('.messages').append('<p>' + message.text + '</p>');
+  jQuery('.messages').append('<p><small>' + '(' + momentTimestamp.local().format('h:mm a') + ')' + '</small>' + ' ' + message.text + '</p>');
 });
 
 // Handles submitting of new message
