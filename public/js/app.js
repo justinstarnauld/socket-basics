88ceb9077a9053f51233d0c238b2fca8addc51a3
Justin St. Arnauld <justin.starnauld@gmail.com>
2016-09-19 22:12:46 -0700

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,23 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+
 var socket = io();
+
+console.log(name + ' wants to join ' + room);
 
 socket.on('connect', function () {
   console.log('Connected to socket.io server!');
 });
 
+
 socket.on('message', function (message) {
   var momentTimestamp = moment.utc(message.timestamp);
+  var $message = jQuery('.messages')
   console.log('New message');
   console.log(message.text);
 
-  jQuery('.messages').append('<p><small>' + '(' + momentTimestamp.local().format('h:mm a') + ')' + '</small>' + ' ' + message.text + '</p>');
+  $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+  $message.append('<p>' + message.text + '</p>')
 });
 
 // Handles submitting of new message
@@ -21,6 +29,7 @@ $form.on('submit', function (event) {
   var $message = $form.find('input[name=message]');
 
   socket.emit('message', {
+    name: name,
     text: $message.val()
   });
 
